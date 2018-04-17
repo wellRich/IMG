e7b5304eb502f35cba37b96937c005e8951a4326
wellRich <kitty_gyk@163.com>
2018-04-17 10:57:45 +0800

    ×Զ¯Ìύ

--- pushToGithub.sh
+++ pushToGithub.sh
@@ -1,15 +1,5 @@
 #! /bin/bash
-echo "Start to publish...\n"
-# 切换到项目目录
-#cd E:/workspace/SERVER
-
-#cd 人口与计生服务管理应用系统/区划代码管理分系统/SERVICE/bapfopm-pfpsmas-zcms-service/src/main/java
-# 执行git命令
-# git pull origin test
-#git config  user.name "wellRich"
-#git config  user.email "kitty_gyk@163.com"
-
-cd E:/workspace/SERVER
+echo "执行git命令...\n"
 git add .
 git commit -m "自动提交"
 git push origin master;
