64558aa0e06af9251c8006118e365e997742c836
wellRich <kitty_gyk@163.com>
2018-04-25 19:14:23 +0800

    鑷姩鎻愪氦

--- bapfopm-pfpsmas-zcms-web/src/modules/zccrfm/js/zccrfm_shangchuan.js
+++
@@ -1,4 +0,0 @@
-define(['avalon','jquery','bootstrap', 'text!./zccrfm_shangchuan.js'], function (avalon,_zccrfm_shangchuan) {
-  avalon.templateCache._zccrfm_shangchuan = _zccrfm_shangchuan;
-
-})
--- bapfopm-pfpsmas-zcms-web/src/modules/zccrfm/js/zccrfm_tongji.js
+++
@@ -1,4 +0,0 @@
-define(['avalon','jquery','bootstrap', 'text!./zccrfm_tongji.js'], function (avalon,_zccrfm_tongji) {
-  avalon.templateCache._zccrfm_tongji = _zccrfm_tongji;
-
-})
